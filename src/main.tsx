e68c9eb7208f5d9bdb7822075ff89c7bb6d3a13d
Lietson Dos Santos <lietsondossantos.me@gmail.com>
2024-03-18 23:31:00 +0100

    :wrench: Configuring aliases

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
 
-import App from './App'
+import App from '@/App'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
